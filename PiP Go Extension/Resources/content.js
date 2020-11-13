882c56625fd48f96056c6aba335f408bc708f409
thoamsy <c6swift@gmail.com>
2020-11-13 18:30:42 +0800

    condition

--- PiP Go Extension/Resources/content.js
+++ PiP Go Extension/Resources/content.js
@@ -1,28 +1,34 @@
 const video = document.querySelector('video');
-const PipButton = document.createElement('button');
-PipButton.textContent = 'Enter PiP';
-PipButton.className = 'ytd-subscribe-button-renderer';
-PipButton.addEventListener('click', () => {
-  PipButton.disabled = true;
-  if (document.pictureInPictureElement === video) {
-    document.exitPictureInPicture();
-    video.play = true;
-    PipButton.disabled = false;
-  } else {
-    video.requestPictureInPicture().finally(() => {
+if (video) {
+  insertPipButton();
+}
+
+function insertPipButton() {
+  const PipButton = document.createElement('button');
+  PipButton.textContent = 'Enter PiP';
+  PipButton.className = 'ytd-subscribe-button-renderer';
+  PipButton.addEventListener('click', () => {
+    PipButton.disabled = true;
+    if (document.pictureInPictureElement === video) {
+      document.exitPictureInPicture();
+      video.play = true;
       PipButton.disabled = false;
-    });
-  }
-});
+    } else {
+      video.requestPictureInPicture().finally(() => {
+        PipButton.disabled = false;
+      });
+    }
+  });
 
-video.addEventListener('leavepictureinpicture', () => {
-  PipButton.textContent = 'Enter Pip';
-});
-video.addEventListener('enterpictureinpicture', (event) => {
-  PipButton.textContent = 'Exit Pip';
-});
+  video.addEventListener('leavepictureinpicture', () => {
+    PipButton.textContent = 'Enter Pip';
+  });
+  video.addEventListener('enterpictureinpicture', (event) => {
+    PipButton.textContent = 'Exit Pip';
+  });
 
-PipButton.style.position = 'relative';
-PipButton.style.zIndex = 10;
-PipButton.style.width = '100px';
-video.parentElement.append(PipButton);
+  PipButton.style.position = 'relative';
+  PipButton.style.zIndex = 10;
+  PipButton.style.width = '100px';
+  video.parentElement.append(PipButton);
+}
